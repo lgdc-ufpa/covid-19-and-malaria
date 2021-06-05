b6adc9a75317ba0e40f93f6c8f74910fdd31a197
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-06-05 10:08:21 -0300

    (lgdc-covid-19-and-malaria.sublime-project, requirements.txt, and sprint_01_data_collection/data_05/geojs-100-mun.json, at /; data_analysis_02.ipynb, and data_analysis_02.py, at sprint_03_data_analysis/; sprint_04_data_visualization/data_visualization_02_covid_cites.ipy) -> data visualization: until 12.1

--- sprint_03_data_analysis/data_analysis_02.py
+++ sprint_03_data_analysis/data_analysis_02.py
@@ -110,3 +110,41 @@ df_cities_cofirmed_cases = pd.concat(df_list_cities_confirmed_cases).reset_index
 output_path = os.path.join(os.getcwd(), 'output', 'output_02_data_02_covid_cities_confirmed_cases.csv')
 
 df_cities_cofirmed_cases.to_csv(output_path, sep=';', decimal='.')
+
+##########
+# Creating df with only confirmed deaths by city
+# - Status: DONE
+
+# 1. Creating list of df with only confirmed deaths by city
+# 2. concat the list into a df_cities_confirmed_deaths
+
+# obs: Using list of df and pd.concat is too far faster than pd.append a df to another one by one
+##############
+
+df_list_cities_confirmed_deaths = []
+
+for cod_city in tqdm(df_cities_sorted['codmun'].unique()):
+
+    # select only cod_city cells
+    df_cod_city = df_cities_sorted[df_cities_sorted['codmun'] == cod_city]
+    
+    # remove lines where accumulated deaths is 0
+    df_cod_city_confirmed_deaths = df_cod_city[df_cod_city['obitosAcumulado'] != 0]    
+    
+    # append to list of df
+    df_list_cities_confirmed_deaths.append(df_cod_city_confirmed_deaths)        
+    
+    # save memory
+    del df_cod_city
+    del df_cod_city_confirmed_deaths
+
+df_cities_cofirmed_deaths = pd.concat(df_list_cities_confirmed_deaths).reset_index(drop=True)
+
+###############################
+# Save df_cities_cofirmed_cases
+# - Status: DONE
+###############################
+
+output_path_confirmed_deaths = os.path.join(os.getcwd(), 'output', 'output_03_data_02_covid_cities_confirmed_deaths.csv')
+
+df_cities_cofirmed_deaths.to_csv(output_path_confirmed_deaths, sep=';', decimal='.')
